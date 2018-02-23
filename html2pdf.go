9e0c8627d69e8eac7664a49b8a5ee79cb1ee411d
yussuf <charu.yussuf@gmail.com>
2018-02-24 01:09:55 +0800

    fix the path

--- html2pdf.go
+++ html2pdf.go
@@ -6,7 +6,7 @@ import (
 	"io/ioutil"
 	"strings"
 
-	"github.com/revenuemonster/rm-api/package/html2pdf/wkhtmltopdf"
+	"github.com/magicwebes/html2pdf/wkhtmltopdf"
 	"github.com/valyala/fasttemplate"
 )
 
